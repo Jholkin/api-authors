8471994855565a68f6843a86bd4681c8fc2dd346
jholkin <jhil.palacios@unas.edu.pe>
2020-09-05 17:40:56 -0500

    poblando datos en la db con seeders

---
+++ database/seeds/AuthorSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class AuthorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i=0; $i <500 ; $i++) { 
+            DB::table('authors')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'github' => $faker->word,
+                'twitter' => '@'.$faker->word,
+                'Location' => $faker->address,
+                'latest_article_published' => $faker->sentence(6,true),
+                'created_at' => date('Y-m-d H:m:s'),
+                'updated_at' => date('Y-m-d H:m:s')
+            ]);
+        }
+    }
+}
